9f736184b4998f079abc0c76e26066a404f254c7
Freddy Silber <freddy.silber@cloudcoach.com>
2022-11-17 16:06:44 -0700

    [chore] cleans up app

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -64,30 +64,33 @@ function App() {
   //   { escape: true }
   // );
 
-  return (
+  if (accounts && accounts.data) {
+    return (
     <div className="App">
-      {
-        accounts && accounts.data
-          ? <Accounts accounts={accounts.data} />
-          : ''
-      }
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
-  );
+        <Accounts accounts={accounts.data} />
+      </div>
+    );
+  } else {
+    return (
+      <div>No Records Found</div>
+    );
+  }
 }
 
 export default App;
+
+
+/* <header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <p>
+    Edit <code>src/App.tsx</code> and save to reload.
+  </p>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+</header> */
