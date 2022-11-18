26cdcaa75d402da836bbe084f9083c89dc37a32e
Freddy Silber <freddy.silber@cloudcoach.com>
2022-11-17 22:18:43 -0700

    [chore] refactors

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Accounts from './components/accounts';
+import restConfig from './auth/rest.config.json';
 
 // // eslint-disable-next-line no-var, @typescript-eslint/naming-convention
 // declare var Visualforce: any;
@@ -11,31 +12,32 @@ function App() {
 
   useEffect(() => {
     // fetch(`/services/apexrest/${defaults.rest.endpoint}`).then((response) => {
-    fetch(
-      // `/services/apexrest/remoting`,
-      `/services/apexrest/project_cloud/remoting`,
-      {
-        method: 'POST',
-        mode: 'cors',
-        headers: new Headers({
-          // Figure out how to get this token from the currently authenticated org
-          'Authorization': 'OAuth 00D1y0000002Ehp!ARwAQJXzqfo9lgysCO_c2E9jsKGQlv9dn1SYzSQCtl6zjsZuCo5Kor9xumJ5G7Twg3z0BiBbQzLyAc9dbmi.yxY3W4HP7iY2',
-          // 'Authorization': 'OAuth 00D9D0000001gFv!ARQAQKUgnmGlSHnveioc0IAHUUjGHzaTOve1g8q23YZBtxUMe.76NOBVOr3LaRDVmtbpGpzIyEtfmDXCpitCqgRhm1q4J92C',
-          'Content-Type': 'application/json',
-        }),
-        credentials: 'include',
-        body: JSON.stringify({
-          apexType: 'c.AccountRemoter.getAccount'
-        }),
-
-      }
-    ).then((response) => {
-      return response.json();
-    }).then((data: Record<any, any>[]) => {
+    async function getAccounts() {
+      const response = await fetch(
+        // `/services/apexrest/remoting`,
+        `/services/apexrest/project_cloud/remoting`,
+        {
+          method: 'POST',
+          mode: 'cors',
+          headers: new Headers({
+            // Figure out how to get this token from the currently authenticated org
+            'Authorization': `OAuth ${restConfig.token}`,
+            // 'Authorization': 'OAuth 00D1y0000002Ehp!ARwAQJXzqfo9lgysCO_c2E9jsKGQlv9dn1SYzSQCtl6zjsZuCo5Kor9xumJ5G7Twg3z0BiBbQzLyAc9dbmi.yxY3W4HP7iY2',
+            // 'Authorization': 'OAuth 00D9D0000001gFv!ARQAQKUgnmGlSHnveioc0IAHUUjGHzaTOve1g8q23YZBtxUMe.76NOBVOr3LaRDVmtbpGpzIyEtfmDXCpitCqgRhm1q4J92C',
+            'Content-Type': 'application/json',
+          }),
+          credentials: 'include',
+          body: JSON.stringify({
+            apexType: 'c.AccountRemoter.getAccount'
+          }),
+        }
+      );
+      console.log(response);
+      const data = response.json();
       setAccounts({ data });
-    }).catch((error) => {
-      console.error(error);
-    });
+    }
+
+    getAccounts();
   }, []);
 
 
