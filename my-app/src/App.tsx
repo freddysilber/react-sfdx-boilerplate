80916bd106cab048422c16c28e1dbbac9c8a9a89
Freddy Silber <freddy.silber@gmail.com>
2022-11-21 16:16:19 -0700

    [feat] styles apps better

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,42 +1,74 @@
-import { Button } from '@salesforce/design-system-react';
+import { Button, Spinner } from '@salesforce/design-system-react';
 import { useEffect, useState } from 'react';
 import './App.css';
 import Accounts from './components/accounts';
-import remotingInvoke from './remoting/remoting';
+import remotingInvoke from './remoting';
+
+export interface SObjectFieldValues {
+  Id: string;
+  Name: string;
+  [x: string]: any;
+}
 
 export default function App() {
-  const [accounts, setAccounts] = useState<any>({});
+  const [state, setState] = useState<Partial<{
+    accounts: SObjectFieldValues[],
+    loading: boolean,
+  }>>({});
+
+  async function getAccounts() {
+    setState({
+      ...state,
+      loading: true,
+    });
+    remotingInvoke<SObjectFieldValues[]>(
+      'AccountRemoter.getAccount',
+      {
+        name: 'params!'
+      },
+    ).then((accounts) => {
+      setState({
+        ...state,
+        accounts
+      });
+    }).catch((error) => {
+      alert(error);
+    });
+  }
+
+  function loading() {
+    if (state.loading) {
+      return (
+        <div style={{ height: '100%' }}>
+          <Spinner
+            size="small"
+            variant="base"
+            assistiveText={{ label: 'Main Frame Loading...' }}
+          />
+        </div>
+      );
+    } else {
+      return null;
+    };
+  }
 
   useEffect(() => {
-    async function getAccounts() {
-      remotingInvoke<any>(
-        'AccountRemoter.getAccount',
-        {
-          name: 'params!'
-        },
-      ).then((accounts) => {
-        setAccounts({
-          data: accounts,
-        });
-      }).catch((error) => {
-        alert(error);
-      });
-    }
-
     getAccounts();
   }, []);
 
-  if (accounts && accounts.data && accounts.data.length) {
-    return (
-      <div className="App">
-        <Button label="Hello Button" />
-        <button className="slds-button slds-button_brand">Button</button>
-        <Accounts accounts={accounts.data} />
+  return (
+    <div className="slds-p-around_small slds-grid slds-grid_vertical">
+      {/* Spinner */}
+      {loading()}
+
+      <div className="slds-box">
+        <Button
+          label="Refresh"
+          variant="brand"
+          onClick={getAccounts}
+        />
+        <Accounts accounts={state.accounts} />
       </div>
-    );
-  } else {
-    return (
-      <div>No Records Found</div>
-    );
-  }
+    </div>
+  );
 }
--- my-app/src/components/accounts.tsx
+++ my-app/src/components/accounts.tsx
@@ -1,16 +1,28 @@
-export default function Accounts(props: any) {
-	return (
-		<div>
-			<h1>Accounts</h1>
-			<span className="slds-badge slds-theme_success">Badge</span>
+import { SObjectFieldValues } from '../App';
 
-			{props.accounts.map((a: any) => {
-				return (
-					<div key={a.Id}>
-						<p>Name: {a.Name}</p>
-					</div>
-				)
-			})}
-		</div>
-	);
+export default function Accounts(props: {
+	accounts: SObjectFieldValues[] | undefined
+}) {
+	if (props.accounts?.length) {
+		return (
+			<div>
+				<span className="slds-badge slds-theme_success slds-m-vertical_medium">{props.accounts.length} Accounts</span>
+
+				{props.accounts.map((a: any) => {
+					return (
+						<div key={a.Id}>
+							<p>
+								<span className="slds-text-color_inverse-weak">Name: </span>
+								<span className='slds-text-title--caps'>{a.Name}</span>
+							</p>
+						</div>
+					)
+				})}
+			</div>
+		);
+	} else {
+		return (
+			<div className="slds-align_absolute-center">No Records Found</div>
+		);
+	}
 }
