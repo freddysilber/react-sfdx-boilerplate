6a1821bf8f7e87b53a65c84dc2ff0ba90f54f94f
Freddy Silber <freddy.silber@gmail.com>
2022-11-21 18:10:45 -0700

    [chore] revert

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
 import { Button, Spinner } from '@salesforce/design-system-react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Accounts from './components/accounts';
 import remotingInvoke from './remoting';
-const Accounts = React.lazy(() => import('./components/accounts'));
 
 export interface SObjectFieldValues {
   Id: string;
